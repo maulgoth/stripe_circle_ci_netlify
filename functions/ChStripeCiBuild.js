61624bdc4725e930e44b419708883d0d304b0ddb
maulgoth <maulgoth@gmail.com>
2021-04-15 10:39:01 -0400

    first

---
+++ functions/ChStripeCiBuild.js
@@ -0,0 +1,31 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async ({ body, headers }) => {
+  try {
+    // check the webhook to make sure it’s valid
+    const stripeEvent = stripe.webhooks.constructEvent(
+      body,
+      headers['stripe-signature'],
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    // only do stuff if this is a successful Stripe Checkout purchase
+    if (stripeEvent.type === 'price.updated') {
+
+      axios.defaults.headers.common['Circle-Token'] = process.env.CIRCLE_API_KEY;
+      axios.post('https://circleci.com/api/v1.1/project/github/gatorpazz/consider-herbs/tree/prods');
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ received: true }),
+    };
+  } catch (err) {
+    console.log(`Stripe webhook failed with ${err}`);
+
+    return {
+      statusCode: 400,
+      body: `Webhook Error: ${err.message}`,
+    };
+  }
+};
