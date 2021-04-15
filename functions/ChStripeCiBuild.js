ea32f119569b3870de21a089ff93ddd094a52396
maulgoth <maulgoth@gmail.com>
2021-04-15 12:20:36 -0400

    all stripe

--- functions/ChStripeCiBuild.js
+++ functions/ChStripeCiBuild.js
@@ -3,18 +3,8 @@ const axios = require('axios');
 
 exports.handler = async ({ body, headers }) => {
   try {
-    // check the webhook to make sure it’s valid
-    // const stripeEvent = stripe.webhooks.constructEvent(
-    //   body,
-    //   headers['stripe-signature'],
-    //   process.env.STRIPE_WEBHOOK_SECRET
-    // );
-
-    // only do stuff if this is a successful Stripe Checkout purchase
-    // if (stripeEvent.type === 'price.updated') {
-
     axios.defaults.headers.common['Circle-Token'] = process.env.CIRCLE_API_KEY;
-    axios.post('https://circleci.com/api/v2/project/github/gatorpazz/consider-herbs/pipeline'
+    axios.post('https://circleci.com/api/v2/project/github/gatorpazz/consider-herbs/pipeline', {"branch": "prods"}
     );
     
     // }
