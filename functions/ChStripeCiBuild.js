2aea2bb6ade1599422e7d579ec57f258ea9beb16
maulgoth <maulgoth@gmail.com>
2021-04-15 12:37:57 -0400

    all stripe

--- functions/ChStripeCiBuild.js
+++ functions/ChStripeCiBuild.js
@@ -2,14 +2,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const axios = require('axios');
 
 exports.handler = async ({ body, headers }) => {
+  let hehe;
   try {
     axios.defaults.headers.common['Circle-Token'] = process.env.CIRCLE_API_KEY;
     axios.post('https://circleci.com/api/v2/project/github/gatorpazz/consider-herbs/pipeline', {branch: "prods"}
     ).then((response) => {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(response)
-      };
+      hehe = response;
     });
   } catch (err) {
     console.log(`Stripe webhook failed with ${err}`);
@@ -21,6 +19,6 @@ exports.handler = async ({ body, headers }) => {
   }
   return {
     statusCode: 200,
-    body: JSON.stringify({ received: true, temp: "YESWEGOTIT" }),
+    body: JSON.stringify({ received: true, temp: hehe }),
   };
 };
