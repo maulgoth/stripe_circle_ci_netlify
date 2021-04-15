df0a59ba2a10833e6a14904099fec3d831305670
maulgoth <maulgoth@gmail.com>
2021-04-15 12:23:49 -0400

    all stripe

--- functions/ChStripeCiBuild.js
+++ functions/ChStripeCiBuild.js
@@ -5,7 +5,12 @@ exports.handler = async ({ body, headers }) => {
   try {
     axios.defaults.headers.common['Circle-Token'] = process.env.CIRCLE_API_KEY;
     axios.post('https://circleci.com/api/v2/project/github/gatorpazz/consider-herbs/pipeline', {"branch": "prods"}
-    );
+    ).then((response) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response);
+      };
+    });
     
     // }
 
